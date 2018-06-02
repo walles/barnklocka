9a110274c546c73e908fa8bed096a9865ca5fe88
Johan Walles <johan.walles@gmail.com>
2018-06-02 20:58:37 +0200

    Turn digital clock into a button
    Click it to speak the digital time.

--- app/src/main/java/com/gmail/walles/johan/johansbarnklocka/MainActivity.java
+++ app/src/main/java/com/gmail/walles/johan/johansbarnklocka/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import java.util.Locale;
 
@@ -32,12 +31,14 @@ public class MainActivity extends AppCompatActivity {
         setTime(analogClock.getHour(), analogClock.getMinute());
 
         final Button analogReadout = findViewById(R.id.analogReadout);
+        final Button digitalClock = findViewById(R.id.digitalClock);
         final Button digitalReadout = findViewById(R.id.digitalReadout);
         tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
                 if (status == TextToSpeech.SUCCESS) {
                     analogReadout.setEnabled(true);
+                    digitalClock.setEnabled(true);
                     digitalReadout.setEnabled(true);
                 }
             }
@@ -46,6 +47,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 tts.speak(analogReadout.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+        digitalClock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Speak the digital readout text
+                tts.speak(digitalReadout.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
             }
         });
         digitalReadout.setOnClickListener(new View.OnClickListener() {
@@ -66,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setTime(int hour, int minute) {
-        final TextView digitalClock = findViewById(R.id.digitalClock);
+        final Button digitalClock = findViewById(R.id.digitalClock);
         digitalClock.setText(
                 String.format(Locale.getDefault(),
                         "%02d:%02d", analogClock.getHour(), analogClock.getMinute()));
